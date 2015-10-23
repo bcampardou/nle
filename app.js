8ed43668a2cd2a09ee9dec286ff2db2184893154
Béranger C <b.campardou@gmail.com>
2015-10-23 17:28:19 +0200

    Express application

--- app.js
+++ app.js
@@ -1,31 +1,60 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var log = require('./routes/log');
+
 var app = express();
 
-var elasticsearch = require('elasticsearch');
-var util = require('util');
-var HttpConnector = require('elasticsearch/src/lib/connectors/http');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-function MyHttpConnector(host, config) {
-  HttpConnector.call(this, host, config);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/log', log);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
-util.inherits(MyHttpConnector, HttpConnector);
-MyHttpConnector.prototype.makeReqParams = function(params) {
-  params = params || {};
-  params.method = params.method || 'POST';
-  return HttpConnector.prototype.makeReqParams.call(this, params);
-};
 
-var client = new elasticsearch.Client({
-  connectionClass: MyHttpConnector
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+module.exports = app;
