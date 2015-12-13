b2d00a3dbbf32c79bafc5332396297cf1e4cc136
BerangerC <b.campardou@gmail.com>
2015-12-13 23:09:56 +0100

    Missing file

---
+++ log-cli.js
@@ -0,0 +1,48 @@
+var uuid = require('node-uuid'),
+  redis = require("redis"),
+  client = redis.createClient();
+
+var help =  "usage: to do...";
+
+var tasks = {};
+
+tasks.help = function(){  
+  console.log(help);
+  process.exit();
+};
+
+
+tasks.apiKeyGen = function(hostname) {
+  console.log("\n------- not implemented ------\n");
+  client.get(hostname, function(error, reply) {
+    if(typeof error !== 'undefined') {
+      throw error;
+    }
+    
+    if(typeof reply === 'undefined') {
+      var apikey = uuid.v4();
+      // Write in redis
+      client.set(hostname, apikey);
+      console.log("Your API Key for '" + hostname + "' is: " + apikey + "\nPlease write it carefully.")
+    } else {
+      throw new Error('A key already exists for the hostname ' + hostname);
+    }
+    process.exit();
+  });
+};
+
+var argv = process.argv.slice(2);
+
+if(argv[0] === "--help" || argv[0] === "-h") {  
+   tasks.help();
+} else if(argv[0] === "keygen") {
+  if(argv[1] === "--hostname" && typeof argv[2] === "string") {
+    var hostname = argv[2];
+    tasks.apiKeyGen(hostname);
+  } else {
+    console.log("You have to specify a hostname with '--hostname' followed by a string")
+  }
+}
+
+
+module.exports = tasks;
